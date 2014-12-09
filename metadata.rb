84b41f0e4ade7e158ced3d52432ee08f521e0456
Artur Nowak <artur.nowak@evidenceprime.com>
2014-12-09 15:41:59 +0100

    Updated metadata for EP fork

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              'couchdb'
-maintainer        'Opscode, Inc.'
-maintainer_email  'cookbooks@opscode.com'
+maintainer        'Artur Nowak'
+maintainer_email  'artur.nowak@evidenceprime.com'
 license           'Apache 2.0'
 description       'Installs CouchDB package and starts service'
 long_description  <<-EOH
@@ -10,7 +10,7 @@ installed via some other method, either a backported package, or compiled
 directly from source. CouchDB is available on Red Hat-based systems through
 the EPEL Yum Repository.
 EOH
-version           '2.5.2'
+version           '2.6.0'
 depends           'erlang'
 depends           'yum', '~> 3.0'
 depends           'yum-epel'
